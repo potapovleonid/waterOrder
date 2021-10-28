046ee54eab821dbb6625bd64fac67ae8ef0e890d
Leonid <kissmykrabeskill@gmail.com>
2021-10-28 20:29:30 +0500

    Init project

---
+++ src/main/java/rosseti/ois/des/waterOrder/WaterOrderApplication.java
@@ -0,0 +1,13 @@
+package rosseti.ois.des.waterOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rosseti/ois/des/waterOrder/WaterOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package rosseti.ois.des.waterOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

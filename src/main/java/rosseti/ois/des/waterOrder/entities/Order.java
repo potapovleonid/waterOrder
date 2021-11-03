1a5f2335b1f72a475d1d16c023b99191ee3539b7
Leonid <kissmykrabeskill@gmail.com>
2021-11-03 17:58:21 +0500

    Edit css Add mapping for order.html and redirect on complete.html Delete User.java Edit Order.java Edit order.html + add post form + print order in a console

--- src/main/java/rosseti/ois/des/waterOrder/entities/Order.java
+++ src/main/java/rosseti/ois/des/waterOrder/entities/Order.java
@@ -1,41 +1,58 @@
-//package rosseti.ois.des.waterOrder.entities;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//public class Order {
-//    @Id
-//    @Column(name = "order_id", nullable = false)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
-//
-//    @Column(name = "quantity_of_water_fld")
-//    private int quantityOfWater;
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
-//
-//    public int getQuantityOfWater() {
-//        return quantityOfWater;
-//    }
-//
-//    public void setQuantityOfWater(int quantityOfWater) {
-//        this.quantityOfWater = quantityOfWater;
-//    }
-//}
+package rosseti.ois.des.waterOrder.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private String full_name;
+    private String address;
+    private Long cabinet;
+
+    private Long quantityOfWater;
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getCabinet() {
+        return cabinet;
+    }
+
+    public void setCabinet(Long cabinet) {
+        this.cabinet = cabinet;
+    }
+
+    public Long getQuantityOfWater() {
+        return quantityOfWater;
+    }
+
+    public void setQuantityOfWater(Long quantityOfWater) {
+        this.quantityOfWater = quantityOfWater;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderTest{" +
+                "full_name='" + full_name + '\'' + "\n" +
+                ", address='" + address + '\'' + "\n" +
+                ", cabinet=" + cabinet + "\n" +
+                ", quantityOfWater=" + quantityOfWater +
+                '}';
+    }
+}

9a0b71bec28752a731e7746fc5a816022e36dc99
Leonid <kissmykrabeskill@gmail.com>
2021-11-04 23:57:15 +0500

    Add entity in Order.java + add postgres link

--- src/main/java/rosseti/ois/des/waterOrder/entities/Order.java
+++ src/main/java/rosseti/ois/des/waterOrder/entities/Order.java
@@ -4,15 +4,38 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @Column(name = "order_id", nullable = false)
+    private Long id;
+
+    @Column(name = "full_name_fld")
     private String full_name;
+
+    @Column(name = "address_fld")
     private String address;
+
+    @Column(name = "cabinet_fld")
     private Long cabinet;
 
+    @Column(name = "quantity_of_water_fld")
     private Long quantityOfWater;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFull_name() {
         return full_name;

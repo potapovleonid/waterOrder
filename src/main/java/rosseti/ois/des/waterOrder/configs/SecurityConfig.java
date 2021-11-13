68662c3002e5201ba392b475df77e517e2d4d2c5
Leonid <kissmykrabeskill@gmail.com>
2021-11-13 23:17:40 +0500

    add spnego beans and dependencies for kerberos

--- src/main/java/rosseti/ois/des/waterOrder/configs/SecurityConfig.java
+++ src/main/java/rosseti/ois/des/waterOrder/configs/SecurityConfig.java
@@ -2,12 +2,19 @@ package rosseti.ois.des.waterOrder.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.kerberos.authentication.KerberosAuthenticationProvider;
+import org.springframework.security.kerberos.authentication.KerberosServiceAuthenticationProvider;
 import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosClient;
+import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosTicketValidator;
+import org.springframework.security.kerberos.web.authentication.SpnegoAuthenticationProcessingFilter;
+import org.springframework.security.kerberos.web.authentication.SpnegoEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import rosseti.ois.des.waterOrder.services.DummyUserDetailsService;
 
 @Configuration
@@ -17,16 +24,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .exceptionHandling()
+                    .authenticationEntryPoint(spnegoEntryPoint())
+                    .and()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .logout().permitAll();
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .logout()
+                    .permitAll()
+                    .and()
+                .addFilterBefore(
+                        spnegoAuthenticationProcessingFilter(authenticationManagerBean()),
+                        BasicAuthenticationFilter.class);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(kerberosAuthenticationProvider());
+        auth.authenticationProvider(kerberosServiceAuthenticationProvider());
     }
 
     @Bean
@@ -35,8 +51,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         SunJaasKerberosClient client = new SunJaasKerberosClient();
         client.setDebug(true);
         provider.setKerberosClient(client);
-//        provider.setUserDetailsService(dummyUserDetailsService());
+        provider.setUserDetailsService(dummyUserDetailService());
         return provider;
+    }
+
+    @Bean
+    public SpnegoEntryPoint spnegoEntryPoint() {
+        return new SpnegoEntryPoint("/login");
+    }
+
+    @Bean
+    public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter(
+            AuthenticationManager authenticationManager) {
+        SpnegoAuthenticationProcessingFilter filter = new SpnegoAuthenticationProcessingFilter();
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
+
+    @Bean
+    public KerberosServiceAuthenticationProvider kerberosServiceAuthenticationProvider() {
+        KerberosServiceAuthenticationProvider provider = new KerberosServiceAuthenticationProvider();
+        provider.setTicketValidator(sunJaasKerberosTicketValidator());
+        provider.setUserDetailsService(dummyUserDetailService());
+        return provider;
+    }
+
+    @Bean
+    public SunJaasKerberosTicketValidator sunJaasKerberosTicketValidator() {
+        SunJaasKerberosTicketValidator validator = new SunJaasKerberosTicketValidator();
+        validator.setServicePrincipal("HTTP/10.81.5.100@rosseti.ural");
+        validator.setKeyTabLocation(new FileSystemResource("/tmp/water.keytab"));
+        validator.setDebug(true);
+        return validator;
     }
 
     @Bean
